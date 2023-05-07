2b2e5610f750c8d72c03fb63dc6f085b01cb4724
VENKATESH CHOPPADHANDI <68506510+venky666666@users.noreply.github.com>
2023-05-07 17:20:05 +0530

    Create Rectangle pattern.py

---
+++ Rectangle pattern.py
@@ -0,0 +1,16 @@
+# n=int(input())
+# for i in range(n):
+#     for j in range(n-1):
+#         print(n-j,end='')
+#     print()
+
+n = int(input())
+s = 1
+for i in range(n):
+    for j in range(n,0,-1):
+        if j == s :
+            print('*',end = '')
+        else:    
+            print(j,end = '')
+    s += 1
+    print()    
